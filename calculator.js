011774a34df766e41b1180675c7a851ce1a7d70c
tom <tommy.surjadi@gmail.com>
2019-01-26 00:28:57 -0800

    Created calculator.js

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let rate = 10;
+let numberHours = 300;
+let tax = 0.1;
+let exchangeRate = 1.4;
+
+console.log(( rate * numberHours + ((rate * numberHours) * tax)) * exchangeRate);
+
+//To calculate total consulting fee based on 100 consulting hours at an hourly rate of USD300 with tax 10% at an exchange rate of 1.4 USD/SGD in Singapore Dollars.//
+
+for (let i = 9; i >= 0; i--){
+    console.log(i)
+  }
+  
+  //Countdown//
+  
