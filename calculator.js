835079ea671403a09e93b916dc4c9ed56cc602c2
tom <tommy.surjadi@gmail.com>
2019-01-27 00:17:14 -0800

    Added more variables and operators in the calculator.js, created countdown.js and canDrive.js

--- calculator.js
+++ calculator.js
@@ -2,13 +2,8 @@ let rate = 300;
 let numberHours = 100;
 let tax = 0.1;
 let exchangeRate = 1.4;
+let downpayment = 5000;
 
-console.log(( rate * numberHours + ((rate * numberHours) * tax)) * exchangeRate);
+console.log(((rate * numberHours - downpayment) * (1 + tax)) * exchangeRate);
 
-//To calculate total consulting fee based on 100 consulting hours at an hourly rate of USD300 with tax 10% at an exchange rate of 1.4 USD/SGD in Singapore Dollars.//
-
-for (let i = 9; i >= 0; i--){
-    console.log(i)
-  }
-  
-  //Countdown//
+//To calculate total consulting invoice based on 100 consulting hours at an hourly rate of USD300 -net of US$5000 downpayment. 10% VAT is to be included in the bill. The bill is quoted in Singapore dollar at an exchange rate of 1.4 USD/SGD.//
---
+++ canDrive.js
@@ -0,0 +1,12 @@
+var firstName = 'James';
+var lastName = 'Norton';
+var age = 42;
+var x = age - 16;
+
+if (age - 16 >= 0) {
+  console.log(firstName +' '+ lastName +' '+ "has been driving for" +' '+ x +' '+'years');
+} else {
+  console.log("Sorry bro, try again next year")
+}
+
+// "James Norton has been driving for 26 years"//
