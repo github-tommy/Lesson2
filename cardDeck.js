f6420960a76a760f8d28c6808700fb440d7cff0e
tom <tommy.surjadi@gmail.com>
2019-01-29 21:42:10 -0800

    Corrected last set of card values to clubs

--- cardDeck.js
+++ cardDeck.js
@@ -4,7 +4,7 @@ var c = ' of diamonds ';
 var d = ' of clubs ';
 
 for(let x = 0; x < 12; x++) {
-  console.log((x+2) + a + ',' + ' ' + (x+2) + b + ',' + ' ' + (x+2) + c + '&' + ' ' + (x+2) + b);
+  console.log((x+2) + a + ',' + ' ' + (x+2) + b + ',' + ' ' + (x+2) + c + '&' + ' ' + (x+2) + d);
 }
 
 //To print the value of all cards//
